0175ae87a3d95bc671fe69392dd171f48c06d206
Khushbu <singhkhushbu210@gmail.com>
2021-05-18 00:51:59 +0530

    js

---
+++ public/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.chat_icon').click(function() {
+		$('.chat_box').toggleClass('active');
+	});
+
+	$('.my-conv-form-wrapper').convform({selectInputStyle: 'disable'})
+});
+
+
+
+
